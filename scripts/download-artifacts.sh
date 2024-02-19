567b33cca542828b09f68c9510a9693c1e9595b9
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-02-19 10:46:54 +0000

    Add script to download artifacts

---
+++ scripts/download-artifacts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pushd .. > /dev/null
+experiment=$(basename $PWD)
+popd > /dev/null
+sub=$(basename $PWD)
+
+if [ "$PRINT_ONLY" != "" ] ; then
+  echo $experiment-$sub.tgz
+  exit 0
+fi
+
+if [ -d data ] ; then
+  echo "data directory already present: cowardly refusing to proceed"
+  exit 1
+fi
+
+mkdir data 2> /dev/null
+wget http://turig.iit.cnr.it/~claudio/public/stateful-faas-sim/$experiment-$sub.tgz -O- | tar zx
