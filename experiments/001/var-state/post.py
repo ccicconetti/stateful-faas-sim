821cbcc9d3135c1eb94bbff0d147954af11c9b3f
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-02-20 13:55:17 +0000

    Experiment 001/var-state: update post script

--- experiments/001/var-state/post.py
+++ experiments/001/var-state/post.py
@@ -8,8 +8,6 @@ df = pd.read_csv("output.csv")
 P_N = 100.0  # Watt
 E_B_values = [0.05, 5]  # uW/b/s
 
-metrics = ["avg-busy-nodes", "total-traffic", "migration-rate", "execution-time"]
-
 for E_B in E_B_values:
     for policy in df["policy"].unique():
         with open(f"{policy}-{E_B}.dat", "w", encoding="utf8") as outfile:
@@ -29,6 +27,5 @@ for E_B in E_B_values:
                 p025 = np.quantile(df_metric.values, 0.025)
                 p975 = np.quantile(df_metric.values, 0.975)
                 line = f"{state_mul} {mean} {p025} {p975}"
-                print(f"{policy} {E_B} {line}")
 
                 outfile.write(f"{line}\n")
