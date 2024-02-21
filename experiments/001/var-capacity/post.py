455e60b788fc3cd6d37492fd1cf7b0a15e2aec92
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-02-21 13:54:11 +0000

    Experiment 001/var-capacity: update post processing add plots

--- experiments/001/var-capacity/post.py
+++ experiments/001/var-capacity/post.py
@@ -9,24 +9,32 @@ P_N = 100.0  # Watt
 
 for policy in df["policy"].unique():
     for defragmentation_interval in df["defragmentation-interval"].unique():
-        with open(
-            f"{policy}-{defragmentation_interval}.dat", "w", encoding="utf8"
-        ) as outfile:
-            for node_capacity in sorted(list(df["node-capacity"].unique())):
+        output = ""
+        for node_capacity in sorted(list(df["node-capacity"].unique())):
+            df_filtered = df.loc[
+                (
+                    (df["policy"] == policy)
+                    & (df["node-capacity"] == node_capacity)
+                    & (df["defragmentation-interval"] == defragmentation_interval)
+                )
+            ]
 
-                df_filtered = df.loc[
-                    (
-                        (df["policy"] == policy)
-                        & (df["node-capacity"] == node_capacity)
-                        & (df["defragmentation-interval"] == defragmentation_interval)
-                    )
-                ]
+            if df_filtered.empty:
+                continue
 
-                # in kWh
-                df_metric = (df_filtered["avg-busy-nodes"] * P_N * 86400) / 3600000
-                mean = df_metric.values.mean()
-                p025 = np.quantile(df_metric.values, 0.025)
-                p975 = np.quantile(df_metric.values, 0.975)
-                line = f"{node_capacity} {mean} {p025} {p975}"
+            # in kWh
+            df_metric = (df_filtered["avg-busy-nodes"] * P_N * 86400) / 3600000
+            mean = df_metric.values.mean()
+            p025 = np.quantile(df_metric.values, 0.025)
+            p975 = np.quantile(df_metric.values, 0.975)
 
-                outfile.write(f"{line}\n")
+            df_metric = df_filtered["avg-busy-nodes"]
+            output += (
+                f"{node_capacity} {mean} {p025} {p975} {df_metric.values.mean()}\n"
+            )
+
+        if output != "":
+            with open(
+                f"{policy}-{defragmentation_interval}.dat", "w", encoding="utf8"
+            ) as outfile:
+                outfile.write(output)
